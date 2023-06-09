2f4cd3f48a862eae75b606c4581830fc19bc0692
vtotiv <vito@dierksen.me>
2023-06-09 17:52:15 +0200

    nvim tree open at startup fix so tree wont always get focussed

--- config/nvim/lua/functions.lua
+++ config/nvim/lua/functions.lua
@@ -1,18 +1,25 @@
+local function isempty(s)
+  return s == nil or s == ''
+end
+
 function open_nvim_tree()
 	-- Get the type of the current buffer
 	local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
 
-	-- current buffer is a file
-	if filetype == 'file' or filetype == 'vim' then
-		require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+	-- nvim opened without a file specified
+	if isempty(filetype) then
+		require("nvim-tree.api").tree.open({ focus = true, find_file = false, })
 		return
 	end
 
-	-- Check if the current buffer is neither a file nor a directory
-	if filetype ~= 'directory' then
-		require("nvim-tree.api").tree.toggle({ focus = true, find_file = false, })
+	-- nvim opened with a given dir
+	if filetype == 'directory' or filetype == "NvimTree" then
+		require("nvim-tree.api").tree.open({ focus = true, find_file = false, })
 		return
 	end
+
+	-- current buffer is a file
+	require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
 end
 
 vim.g.mapleader = ' '
@@ -31,3 +38,5 @@ function nvim_tree_on_attach(bufnr)
 	vim.keymap.set('n', '<Leader>h', api.node.open.horizontal, opts('Open: Horizontal Split'))
 	vim.keymap.set('n', '<Leader>c', api.tree.change_root_to_node, opts('CD'))
 end
+
+
