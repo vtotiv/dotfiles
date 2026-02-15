276c5434cf0d72565774fb7fb8699c9726771d3c
vtotiv <vito@dierksen.me>
2026-02-15 13:47:37 +0100

    added stylua as lua formatter

--- config/nvim/lua/autocmd.lua
+++ config/nvim/lua/autocmd.lua
@@ -1,11 +1,17 @@
 -- Quit when nvim-tree is last buffer and ask for unsaved files
 vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
-  pattern = "NvimTree_*",
-  callback = function()
-    local layout = vim.api.nvim_call_function("winlayout", {})
-    if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
-  end
+	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+	pattern = "NvimTree_*",
+	callback = function()
+		local layout = vim.api.nvim_call_function("winlayout", {})
+		if
+			layout[1] == "leaf"
+			and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+			and layout[3] == nil
+		then
+			vim.cmd("confirm quit")
+		end
+	end,
 })
 
 -- open nvim-tree on startup when no name or file, but don't focus if file given
@@ -13,8 +19,8 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- Format on save with conform.nvim
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json", },
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json", "*.py", "*.pyx", "*.yaml", "*.yml", "*.graphql", "*.lua" },
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
--- config/nvim/lua/formatter.lua
+++ config/nvim/lua/formatter.lua
@@ -1,9 +1,13 @@
 require("conform").setup({
-  formatters_by_ft = {
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescriptreact = { "prettier" },
-    json = { "prettier" },
-  },
+	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		json = { "prettier" },
+		graphql = { "prettier" },
+		python = { "black" },
+		yaml = { "prettier" },
+		lua = { "stylua" },
+	},
 })
